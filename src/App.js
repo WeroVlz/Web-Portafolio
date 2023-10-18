2aa09f4dce2ab67940aff95fc385027bff807f09
Edgar <edgarvlzm26@gmail.com>
2023-10-17 21:28:50 -0600

    NavBar fix and padding corrected

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import NavBar from './components/nav-bar/NavBar';
 import Home from './components/pages/home/Home';
 import About from './components/pages/about/About';
-import Projects from './components/pages/Projects';
+import Projects from './components/pages/projects/Projects';
 import Gallery from './components/pages/Gallery';
 import Resume from './components/pages/resume/Resume';
 import {Route, Routes} from "react-router-dom"
@@ -11,7 +11,9 @@ function App() {
 
   return (
     <div className="App">
-      <NavBar />
+      <div className='app-nav-bar'>
+        <NavBar />
+      </div>
       <div className='container'>
         <Routes>
           <Route path="/" element={<Home />}/>
---
+++ src/components/pages/projects/Projects.js
@@ -0,0 +1,11 @@
+import './Projects.css'
+
+export default function Projects(){
+    return(
+        <div className="project-container">
+            <div className='side-bar'>
+
+            </div>
+        </div>
+    )
+}
