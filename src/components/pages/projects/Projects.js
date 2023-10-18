44d2796813af95df7ee45d47c359a8d231136d17
Edgar <edgarvlzm26@gmail.com>
2023-10-18 03:15:38 -0600

    Projects scroll bar now fully works

--- src/components/pages/projects/Projects.js
+++ src/components/pages/projects/Projects.js
@@ -7,6 +7,10 @@ import RayTracer from '../../../assets/ray-edgar.png'
 import ChatRoom from '../../../assets/lobby.jpeg'
 import CarDashboard from '../../../assets/dashboard.png'
 import TravelAgency from '../../../assets/agency.png'
+import BipedModel from '../../../assets/biped.png'
+import GameAssets from '../../../assets/gameassets.jpg'
+import SnowShader from '../../../assets/snow.png'
+import { useState } from 'react'
 
 const edgar = {
     image: Edgar,
@@ -41,21 +45,42 @@ const edgar = {
 
 const carlos = {
     image: Carlos,
-    name: "Carlos"
+    name: "Carlos",
+    projects: [
+        {
+            icon: SnowShader,
+            name: "Snow shader",
+            program: "Unity",
+            description: "A shader made with Unity’s shader graph that adds snow to any object and lets you choose the color of the object, the color of the snow and the deph of the snow."
+        },
+        {
+            icon: GameAssets,
+            name: "Video game assets",
+            program: "Blender",
+            description: "29 objects + fully-rigged player model modeled and texture painted from scratch in Blender. All of this was made for a social service project, and it was made while learning Blender at the same time." 
+        },
+        {
+            icon: BipedModel,
+            name: "Biped 3D Model",
+            program: "Maya",
+            description: "A 2 months work in progress rig of a model of a male human being with IK and FK arms and legs that allows you to switch between the two. It also has eye tracker for both eyes and for each individually."
+        }
+    ]
 }
 
 export default function Projects() {
 
-    const projectsEdgar = edgar.projects;
+    const [selectedPerson, setSelectedPerson] = useState(1);
+    const persons = [edgar, carlos]
 
     return (
         <div className="project-container">
             <div className='side-bar'>
-                <PhotoCard data={edgar} />
-                <PhotoCard data={carlos} />
+                <PhotoCard data={edgar} onClick={() => setSelectedPerson(0)}/>
+                <PhotoCard data={carlos} onClick={() => setSelectedPerson(1)}/>
             </div>
             <ul className='project-carousel'>
-                {projectsEdgar.map((project, index) => (
+                {persons[selectedPerson].projects.map((project, index) => (
                     <ProjectCard 
                         key={index}
                         data={project}
--- src/components/pages/projects/photo-card/PhotoCard.jsx
+++ src/components/pages/projects/photo-card/PhotoCard.jsx
@@ -1,12 +1,12 @@
 import './PhotoCard.css'
 
-export default function PhotoCard({data}){
+export default function PhotoCard({data, onClick}){
 
     const{image, name} = data;
 
     return(
         <div className="photo-card">
-            <img src={image}></img>
+            <img src={image} onClick={onClick}></img>
             <h1 className='name'>{name}</h1>
         </div>
     )
